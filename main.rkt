5abfcf966a1bd90a165c22842a7fcf1346b43e1c
Antigen-1 <zhanghao@antigen.top>
2023-01-11 16:23:55 +0800

    add accumulator and filter

--- main.rkt
+++ main.rkt
@@ -9,13 +9,21 @@
 
 (define reverse-tree (lambda (l) (let loop ((l l) (r empty-tree)) (cond ((empty-tree? l) r) (else (loop (others l) (add-to-tree (first l) r)))))))
 
+;;accumulator
+(define accumulate-leaves 
+  (lambda (tree op init)
+    (let loop ((t tree) (r init))
+      (cond ((empty-tree? t) r)
+            ((tree? (first t)) (loop (others t) (loop (first t) r)))
+            (else (loop (others t) (op (first t) r)))))))
+;;filter
+(define filter-leaves
+  (lambda (tree pred)
+    (reverse-tree (accumulate-leaves tree (lambda (item new) (if (tree? item) (add-to-tree (filter-leaves item pred) new) (if (pred item) (add-to-tree item new) new))) empty-tree))))
+;;transducers
 (define map-leaves
-  (lambda (tree proc)
-    (let loop ((tree tree) (result empty-tree))
-      (cond ((empty-tree? tree) (reverse-tree result))
-            ((tree? (first tree)) (loop (others tree) (add-to-tree (loop (first tree) empty-tree) result)))
-            (else (loop (others tree) (add-to-tree (proc (first tree)) result)))))))
-
+  (lambda (tree proc) 
+    (reverse-tree (accumulate-leaves tree (lambda (item new) (add-to-tree (if (tree? item) (map-leaves item proc) (proc item)) new)) empty-tree))))
 (define select-and-map-leaves
   (lambda (tree proc pred)
     (map-leaves tree (lambda (leaf) (if (pred leaf) (proc leaf) leaf)))))
