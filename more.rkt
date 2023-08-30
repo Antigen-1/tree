aade003534abca92eb86722fef57596a439a6633
Antigen-1 <zhanghao@antigen.top>
2023-08-30 13:45:04 +0800

    add counters and update `fold/breadth-first`

--- more.rkt
+++ more.rkt
@@ -2,8 +2,12 @@
 (require "base.rkt" racket/list racket/contract)
 (provide (contract-out (fold/with-depth (->* ((-> any/c any/c any/c any) any/c tree?) (exact-nonnegative-integer?) any))
                        
-                       (fold/depth-first fold/c)
-                       (fold/breadth-first fold/c)
+                       (fold/depth-first (-> (-> any/c any/c any) any/c tree? any))
+                       (fold/breadth-first (-> (-> any/c any/c any) any/c tree? ... any))
+
+                       (count-nodes (-> tree? any))
+                       (count-leaves (-> tree? any))
+                       (count-levels (-> tree? any))
 
                        (print-tree (->* (tree?) (#:print (-> any/c any) #:char char? #:init-num exact-nonnegative-integer?) any))))
 
@@ -12,17 +16,28 @@
   (define new-depth (add1 depth))
   (for/fold ((r (proc (label tree) init depth))) ((b (in-list (branches tree))))
     (fold/with-depth proc r b new-depth)))
-
-(define fold/c (-> (-> any/c any/c any) any/c tree? any))
 (define (fold/depth-first proc init tree)
   (fold/with-depth (lambda (l i _) (proc l i)) init tree))
-(define (fold/breadth-first proc init tree)
-  (define (loop result tree-list)
-    (cond ((null? tree-list) result)
-          (else
-           (loop (for/fold ((r result)) ((t (in-list tree-list))) (proc (label t) r))
-                 (append* (map branches tree-list))))))
-  (loop init (list tree)))
+(define (fold/breadth-first proc init . trees)
+  (cond ((null? trees) init)
+        (else
+         (apply
+          fold/breadth-first
+          proc
+          (for/fold ((r init)) ((t (in-list trees))) (proc (label t) r))
+          (append* (map branches trees))))))
+
+;;counters
+(define (count-nodes tree)
+  (fold/depth-first (lambda (_ i) (add1 i)) 0 tree))
+(define (count-leaves tree)
+  (cond ((leaf? tree) 1)
+        (else (for/fold ((r 0)) ((b (in-list (branches tree)))) (+ r (count-leaves b))))))
+(define (count-levels tree)
+  (let* ((maximum (box 0))
+         (set (lambda (l i num) (cond ((> num (unbox maximum)) (set-box! maximum num))))))
+    (fold/with-depth set (void) tree 1)
+    (unbox maximum)))
 
 ;;a logger
 (define (print-tree tree #:print (print display) #:char (char #\ ) #:init-num (init-number 0))
@@ -40,7 +55,11 @@
   ;;accumulators
   (check-true (= (fold/with-depth (lambda (l i d) (+ i (* l d))) 0 t) 12))
   (check-true (equal? (fold/depth-first (lambda (l i) (cons l i)) null t) '(4 3 2 1)))
-  (check-true (equal? (fold/breadth-first (lambda (l i) (cons l i)) null t) '(3 4 2 1)))
+  (let ((t1 (tree 5)))
+    (check-true (equal? (fold/breadth-first (lambda (l i) (cons l i)) null t t1) '(3 4 2 5 1))))
+  (check-true (= (count-nodes t) 4))
+  (check-true (= (count-leaves t) 2))
+  (check-true (= (count-levels t) 3))
   ;;the logger
   (check-true (string=? (with-output-to-string (lambda () (print-tree t
                                                                       #:print (lambda (o) (display (format "&~a" o)))
