911d5a52857e8a2e22b66d4e634887225a4e7445
Antigen-1 <zhanghao@antigen.top>
2023-09-01 09:59:56 +0800

    generate flat contracts when possible

--- base.rkt
+++ base.rkt
@@ -24,18 +24,26 @@
   (null? (branches t)))
 ;;a contract constructor for its labels
 (define (treeof element/c)
-  (define c (cons/c element/c (recursive-contract (listof c)))) c)
+  (define c (cons/c element/c
+                    (if (flat-contract? element/c)
+                        (recursive-contract (listof c) #:flat)
+                        (recursive-contract (listof c)))))
+  c)
 
 (module* representation-tests racket/base
   (require racket/contract rackunit (submod ".."))
 
+  ;;the constructor
   (check-exn exn:fail:contract? (lambda () (tree 1 2 3)))
-
   (define t1 (tree 1))
   (define t2 (tree 2 t1))
 
-  (check-not-exn (lambda () (contract (treeof exact-positive-integer?) t2 'pos 'neg)))
-  (check-exn exn:fail:contract? (lambda () (contract (treeof exact-positive-integer?) (tree "a") 'pos 'neg)))
+  ;;contracts
+  (check-true ((treeof exact-positive-integer?) t2))
+  (check-false ((treeof exact-positive-integer?) (tree "a")))
+  (check-exn exn:fail:contract? (lambda () ((treeof (-> any/c any)) add1)))
+  ;;predicates
   (check-true (and (tree? t1) (tree? t2) (leaf? t1) (not (leaf? t2))))
+  ;;selectors
   (check-true (and (= 1 (label t1)) (= 2 (label t2))))
   (check-true (eq? t1 (car (branches t2)))))
